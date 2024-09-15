55f0f149661315d08f6637029be7077c3c24906d
AlexanderTheGreat26 <kaegod@yandex.ru>
2024-09-15 15:54:19 +0300

    Predictor-corrector method implemented

--- main.cpp
+++ main.cpp
@@ -21,30 +21,27 @@ typedef std::array<double, 4> coeff;
 typedef std::vector<double> grid;
 
 
-void new_node (grid & velocities, grid & coordinates, const double & h);
+void Runge_Kutta_method (grid & velocities, grid & coordinates, const double & h);
+
+void Euler_Cauchy_method (grid & velocities, grid & coordinates, const double & h);
+
+void method_implementation (grid & times, grid coordinates, grid velocities, const double & mesh_step,
+                            const std::string & method_name, void method(grid & velocities, grid & coordinates, const double & h));
 
 grid mesh (double left_border, const double & right_border, const double & mesh_step);
-
-void data_file_creation (const std::string & name, grid & x, grid & y, grid & z);
-
-void plot (const std::string & name, const std::string & title,
-           const std::string & xlabel, const std::string & ylabel, const std::string & x_column, const std::string & y_column);
 
 
 int main() {
     grid coordinates {x_0}, velocities {v_0};
     grid times = std::move(mesh(t_0, t_N, step));
-    for (int i = 0; i < times.size(); ++i)
-        new_node(velocities, coordinates, step);
-    data_file_creation("result", times, coordinates, velocities);
-    plot("result", "Solution", "t", "x", "1", "2");
-    plot("result", "Phase portrait", "x", "v", "2", "3");
+    method_implementation(times, coordinates, velocities, step, "RKM", Runge_Kutta_method);
+    method_implementation(times, coordinates, velocities, step, "PCM", Euler_Cauchy_method);
     return 0;
 }
 
 
 double velocity (double x, double v) {
-    return -2*l*v - std::pow(w_0, 2) * x;
+    return -2.0*l*v - std::pow(w_0, 2) * x;
 }
 
 
@@ -65,7 +62,7 @@ void coefficients (const double & h, double & x_n, double & v_n, coeff & k, coef
 }
 
 
-void new_node (grid & velocities, grid & coordinates, const double & h) {
+void Runge_Kutta_method (grid & velocities, grid & coordinates, const double & h) {
         coeff k, q;
         long n = velocities.size()-1;
         double x_n = coordinates[n];
@@ -73,6 +70,27 @@ void new_node (grid & velocities, grid & coordinates, const double & h) {
         coefficients(h, x_n, v_n, k, q, coordinate, velocity);
         coordinates.emplace_back(x_n + h/6.0*(q[0] + 2.0*q[1] + 2.0*q[2] + q[3]));
         velocities.emplace_back(v_n + h/6.0*(k[0] + 2.0*k[1] + 2.0*k[2] + k[3]));
+}
+
+
+double predictor (double & x_n, double & y_n, const double & h, double f(double x, double y)) {
+    return y_n + h * f(x_n, y_n);
+}
+
+double corrector (double & x_n, double & y_n, double & y_pred, const double & h, double f(double x, double y)) {
+    return y_n + h/2.0*(f(x_n, y_n) + f(x_n, y_pred));
+}
+
+
+void Euler_Cauchy_method (grid & velocities, grid & coordinates, const double & h) {
+    long n = velocities.size()-1;
+    double x_n = coordinates[n];
+    double v_n = velocities[n];
+    double v_pred = predictor(x_n, v_n, h, velocity);
+    double v_new = corrector (x_n, v_n, v_pred, h, velocity);
+    double x_new = x_n + h * v_new;
+    velocities.emplace_back(v_new);
+    coordinates.emplace_back(x_new);
 }
 
 
@@ -128,21 +146,11 @@ void plot (const std::string & name, const std::string & title, const std::strin
 }
 
 
-/*coeff coefficients (const double & h, double & x_n, double & v_n, double f (double x, double v)) {
-    coeff k;
-    k[0] = f(x_n, v_n);
-    k[1] = f(x_n + h / 2.0, v_n + h / 2.0 * k[0]);
-    k[2] = f(x_n + h / 2.0, v_n + h / 2.0 * k[1]);
-    k[3] = f(x_n + h, v_n + h * k[2]);
-    return k;
+void method_implementation (grid & times, grid coordinates, grid velocities, const double & mesh_step,
+                            const std::string & method_name, void method(grid & velocities, grid & coordinates, const double & h)) {
+    for (int i = 0; i < times.size(); ++i)
+        method(velocities, coordinates, mesh_step);
+    data_file_creation(method_name, times, coordinates, velocities);
+    plot(method_name, "Solution " + method_name, "t", "x", "1", "2");
+    plot(method_name, "Phase portrait " + method_name, "x", "v", "2", "3");
 }
-
-void new_node (const double & h, grid & coordinates, grid & velocities) {
-    long n = velocities.size()-1;
-    coeff k = coefficients(h, coordinates[n], velocities[n], velocity);
-    velocities.emplace_back(velocities[n] + h/6.0*(k[0] + 2*k[1] + 2*k[2] + k[3]));
-    coeff q = coefficients(h, coordinates[n], velocities[n+1], coordinate);
-    coordinates.emplace_back(coordinates[n] + h/6.0*(q[0] + 2*q[1] + 2*q[2] + q[3]));
-}*/
-
-
